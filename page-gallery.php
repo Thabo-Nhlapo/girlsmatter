4ea3326acbe260ecf5c80a71b01534bba66572fb
TebogoMlondobozi <42371169+TebogoMlondobozi@users.noreply.github.com>
2018-08-30 23:36:26 +0200

    These are the gallery, about and contact pages

---
+++ page-gallery.php
@@ -0,0 +1,94 @@
+<?php get_header(); ?>
+
+<!-- Page top section start -->
+<div class="page-top-area set-bg" data-setbg="https://images.pexels.com/photos/953213/pexels-photo-953213.jpeg?cs=srgb&dl=computer-device-display-953213.jpg&fm=jpg">
+  <div class="breadcrumb-area">
+    <a href="http://localhost/wordpress/">Home</a> / <span>Portfolio</span>
+  </div>
+</div>
+<!-- Page top section end -->
+
+
+<!-- Portfolio section start -->
+<section class="portfolio-section">
+
+  <!-- portfolio filter menu -->
+  <!-- <div class="sp-pad spad">
+    <ul class="portfolio-filter controls">
+      <li class="control" data-filter="*">All</li>
+      <li class="control" data-filter=".photo">Photography</li>
+      <li class="control" data-filter=".wedding">Weddings</li>
+      <li class="control" data-filter=".land">Landscapes</li>
+      <li class="control" data-filter=".port">Portraits</li>
+    </ul>
+  </div> -->
+  <div class="portfolio-warp">
+    <!-- single item -->
+
+    <div class="mix single-portfolio set-bg photo" data-setbg="https://images.pexels.com/photos/1036620/pexels-photo-1036620.jpeg?cs=srgb&dl=adult-attractive-beautiful-1036620.jpg&fm=jpg">
+      <a href="#" class="portfolio-info">
+        <div class="pfbg set-bg" data-setbg="https://images.pexels.com/photos/1036620/pexels-photo-1036620.jpeg?cs=srgb&dl=adult-attractive-beautiful-1036620.jpg&fm=jpg"></div>
+        <h5>Pink dreams</h5>
+        <p>Portraits</p>
+      </a>
+    </div>
+    <!-- single item -->
+    <div class="mix single-portfolio set-bg wedding" data-setbg="https://images.pexels.com/photos/428333/pexels-photo-428333.jpeg?cs=srgb&dl=blur-boy-casual-428333.jpg&fm=jpg">
+      <a href="#" class="portfolio-info">
+        <div class="pfbg set-bg" data-setbg="https://images.pexels.com/photos/428333/pexels-photo-428333.jpeg?cs=srgb&dl=blur-boy-casual-428333.jpg&fm=jpg"></div>
+        <h5>Casually elegant</h5>
+        <p>Portraits</p>
+      </a>
+    </div>
+    <!-- single item -->
+    <div class="mix single-portfolio set-bg land" data-setbg="https://images.pexels.com/photos/871495/pexels-photo-871495.jpeg?cs=srgb&dl=adult-attractive-beautiful-871495.jpg&fm=jpg">
+      <a href="#" class="portfolio-info">
+        <div class="pfbg set-bg" data-setbg="https://images.pexels.com/photos/871495/pexels-photo-871495.jpeg?cs=srgb&dl=adult-attractive-beautiful-871495.jpg&fm=jpg"></div>
+        <h5>Floral summer glow</h5>
+        <p>Portraits</p>
+      </a>
+    </div>
+    <!-- single item -->
+    <div class="mix single-portfolio sm-wide set-bg port" data-setbg="https://images.pexels.com/photos/604146/pexels-photo-604146.jpeg?cs=srgb&dl=beautiful-eyes-face-604146.jpg&fm=jpg">
+      <a href="#" class="portfolio-info">
+        <div class="pfbg set-bg" data-setbg="https://images.pexels.com/photos/604146/pexels-photo-604146.jpeg?cs=srgb&dl=beautiful-eyes-face-604146.jpg&fm=jpg"></div>
+        <h5>Street life</h5>
+        <p>Portraits</p>
+      </a>
+    </div>
+    <!-- single item -->
+    <div class="mix single-portfolio sm-wide set-bg photo" data-setbg="https://images.pexels.com/photos/932397/pexels-photo-932397.jpeg?cs=srgb&dl=adult-beautiful-blur-932397.jpg&fm=jpg">
+      <a href="#" class="portfolio-info">
+        <div class="pfbg set-bg" data-setbg="https://images.pexels.com/photos/932397/pexels-photo-932397.jpeg?cs=srgb&dl=adult-beautiful-blur-932397.jpg&fm=jpg"></div>
+        <h5>Own the edge</h5>
+        <p>Portraits</p>
+      </a>
+    </div>
+    <!-- single item -->
+    <div class="mix single-portfolio set-bg wedding" data-setbg="https://images.pexels.com/photos/819115/pexels-photo-819115.jpeg?cs=srgb&dl=african-beautiful-beauty-819115.jpg&fm=jpg">
+      <a href="#" class="portfolio-info">
+        <div class="pfbg set-bg" data-setbg="https://images.pexels.com/photos/819115/pexels-photo-819115.jpeg?cs=srgb&dl=african-beautiful-beauty-819115.jpg&fm=jpg"></div>
+        <h5>Sun kissed</h5>
+        <p>Portraits</p>
+      </a>
+    </div>
+    <div class="mix single-portfolio sm-wide set-bg port" data-setbg="https://images.pexels.com/photos/1165999/pexels-photo-1165999.jpeg?cs=srgb&dl=beautiful-daylight-facial-expression-1165999.jpg&fm=jpg">
+      <a href="#" class="portfolio-info">
+        <div class="pfbg set-bg" data-setbg="https://images.pexels.com/photos/1165999/pexels-photo-1165999.jpeg?cs=srgb&dl=beautiful-daylight-facial-expression-1165999.jpg&fm=jpg"></div>
+        <h5>My own skin</h5>
+        <p>Portraits</p>
+      </a>
+    </div>
+
+  <div class="clearfix"></div>
+  <!-- <div class="sp-pad next-portfolio-link">
+    <a href="#" class="arrow-btn">
+      <i class="fa fa-angle-right"></i>
+      <i class="fa fa-angle-right"></i>
+      <i class="fa fa-angle-right"></i>
+    </a>
+  </div> -->
+</section>
+<!-- Portfolio section end -->
+
+  <?php get_footer(); ?>
