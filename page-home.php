2408b01e0c6c41f4c976cf4b0ddc099cfc307be8
SinethembaMatwa <42338769+SinethembaMatwa@users.noreply.github.com>
2018-08-30 23:43:31 +0200

    Stylesheet, Content, Homepage, Single

---
+++ page-home.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+
+  <!-- Hero section start -->
+		<div class="hero-slider owl-carousel">
+			<div class="hs-item set-bg sp-pad" data-setbg="https://images.pexels.com/photos/1168981/pexels-photo-1168981.jpeg?cs=srgb&dl=adventure-daylight-desktop-wallpaper-1168981.jpg&fm=jpg">
+				<div class="hs-text">
+					<h2 class="hs-title">TN</h2>
+					<p class="hs-des">Cutting Edge <br>Designs, always.</p>
+				</div>
+			</div>
+			<div class="hs-item set-bg sp-pad" data-setbg="https://images.pexels.com/photos/988870/pexels-photo-988870.jpeg?cs=srgb&dl=antioxidant-berries-bilberry-988870.jpg&fm=jpg">
+				<div class="hs-text">
+					<h2 class="hs-title">TN</h2>
+					<p class="hs-des">Ring Me Up, <br>Let's Create!</p>
+				</div>
+			</div>
+		</div>
+	<!-- Hero section end -->
+
+
+	<!-- Slider Wrapper -->
+<!-- <div class="css-slider-wrapper">
+  <div class="slider slide-1">
+    <img src="img/hero-slider/1.jpg" alt="">
+    <div class="slider-content">
+      <h4>New Product</h4>
+      <h2>Denim Longline T-Shirt Dress With Split</h2>
+      <button type="button" class="buy-now-btn" name="button">$130</button>
+    </div>
+  </div>
+</div> -->
+
+<?php get_footer(); ?>
